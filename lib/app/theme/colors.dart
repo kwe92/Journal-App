16c84b42202bcbe34d04485d7413217ff513ced9
Kwe <Kweayon@gmail.com>
2023-11-23 14:10:04 -0600

    update: added colors

--- lib/app/theme/colors.dart
+++ lib/app/theme/colors.dart
@@ -2,5 +2,10 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   const AppColors._();
-  static const offGrey = Color(0xffF5F5F5);
+  static const offGrey = Color(0xff445D67);
+  static const offWhite = Color(0xffF5F5F5);
+  static const blue0 = Color(0xff1e91ff);
+  static const blueGrey0 = Color(0xff545F68);
+  // static const diaryBackground = Color(0xff00d2ff);
+  static const diaryBackground = Color(0xffffffff);
 }
