e27a79f945c7e98b5c3cc4d55b08bc45681c4ac6
Kwe <Kweayon@gmail.com>
2023-11-23 14:10:20 -0600

    update: expanded text theme

--- lib/app/theme/theme.dart
+++ lib/app/theme/theme.dart
@@ -1,9 +1,31 @@
+import 'package:diary_app/app/theme/colors.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
   const AppTheme._();
 
   static ThemeData getTheme() {
-    return ThemeData(useMaterial3: true);
+    return ThemeData(
+      useMaterial3: true,
+      appBarTheme: appBarTheme,
+      textTheme: textTheme,
+    );
   }
 }
+
+const appBarTheme = AppBarTheme(
+  backgroundColor: AppColors.offGrey,
+);
+
+final textTheme = TextTheme(
+  titleLarge: TextStyle(
+    foreground: Paint()..color = AppColors.offWhite,
+    fontSize: 28,
+    fontWeight: FontWeight.w700,
+  ),
+  bodyMedium: const TextStyle(
+    // foreground: Paint()..color = AppColors.offWhite,
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+  ),
+);
