90b8d75526ce5086b30ca4dfd2ede4df20f94b34
Kwe <Kweayon@gmail.com>
2023-11-24 13:32:26 -0600

    feat: added http client to singleton registration

--- lib/features/shared/services/get_it.dart
+++ lib/features/shared/services/get_it.dart
@@ -1,8 +1,11 @@
 import 'package:journal_app/app/app_router.dart';
 import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
 
 final locator = GetIt.instance;
 
+/// configureDependencies initalizes and registers all GetIt singletons.
 void configureDependencies() {
   locator.registerSingleton<AppRouter>(AppRouter());
+  locator.registerSingleton<http.Client>(http.Client());
 }
