ccbdaae8ac4f96c520fd4ddf7a8b68b30b836364
Kwe <Kweayon@gmail.com>
2023-11-25 18:29:23 -0600

    feat: text form fields for signin

---
+++ lib/features/authentication/ui/signIn/widgets/email_input.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:journal_app/features/authentication/ui/signIn/signin_view_model.dart';
+import 'package:stacked_hooks/stacked_hooks.dart';
+
+// TODO: add comments | StackedHookView | flutter hooks
+// TODO: add leading icon to clear text
+
+class EmailInput extends StackedHookView<SignInViewModel> {
+  final FocusNode focus;
+  final FocusNode? nextFocus;
+
+  const EmailInput({
+    required this.focus,
+    this.nextFocus,
+    super.key,
+  });
+
+  @override
+  Widget builder(BuildContext context, SignInViewModel model) {
+    // useTextEditingController: flutter hook that allows the use of TextEditingController's in a stateless widget
+    final TextEditingController emailController = useTextEditingController();
+
+    return TextFormField(
+      // move to next focus node
+      textInputAction: TextInputAction.next,
+      controller: emailController,
+      focusNode: focus,
+      // commonly used hints
+      autofillHints: const [AutofillHints.email],
+      // TODO: add validator: replace with string service and a call to string validator
+      // validator: (String? value) {},
+      onChanged: model.setEmail,
+      onEditingComplete: () => (nextFocus != null) ? nextFocus!.requestFocus() : focus.unfocus(),
+      decoration: const InputDecoration(
+        labelText: "Email",
+        hintText: "Enter Email Address",
+      ),
+    );
+  }
+}
+
+// StackedHookView
+
+//   - returns a model in the builder function that was passed as a parameterized type
+//   - allows the use of flutter hooks as well
+
+// Flutter Hooks | package
+
+//   - minimize the amount of bolierplate code that a stateful widget would require
+//     to work with various controllers
+//   - enables the use of controllers within a stateful widget
---
+++ lib/features/authentication/ui/signIn/widgets/password_input.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:journal_app/features/authentication/ui/signIn/signin_view_model.dart';
+import 'package:stacked_hooks/stacked_hooks.dart';
+
+// TODO: add leading icon to clear text
+
+class PasswordInput extends StackedHookView<SignInViewModel> {
+  final FocusNode focus;
+  final FocusNode? nextFocus;
+
+  const PasswordInput({
+    required this.focus,
+    this.nextFocus,
+    super.key,
+  });
+
+  @override
+  Widget builder(BuildContext context, SignInViewModel model) {
+    final TextEditingController passwordController = useTextEditingController();
+
+    return TextFormField(
+      textInputAction: TextInputAction.done,
+      controller: passwordController,
+      focusNode: focus,
+      autofillHints: const [AutofillHints.password],
+      // TODO: add validator: replace with string service and a call to string validator
+      // validator: (String? value) {
+      // },
+      obscureText: model.obscurePassword,
+      onChanged: model.setPassword,
+      onEditingComplete: () => (nextFocus != null) ? nextFocus!.requestFocus() : focus.unfocus(),
+      decoration: InputDecoration(
+        labelText: "Password",
+        hintText: "Enter Password",
+        suffixIcon: IconButton(
+          onPressed: () => model.setObscure(!model.obscurePassword),
+          icon: Icon(model.obscurePassword ? Icons.visibility_off : Icons.visibility),
+        ),
+      ),
+    );
+  }
+}
