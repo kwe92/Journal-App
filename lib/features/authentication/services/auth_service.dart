43da7d2dca6bfd24f7229029b50c7476870fb569
Kwe <Kweayon@gmail.com>
2023-11-27 10:57:37 -0600

    feat: updated comments and added isLoggedIn variable

--- lib/features/authentication/services/auth_service.dart
+++ lib/features/authentication/services/auth_service.dart
@@ -5,21 +5,36 @@ import 'package:journal_app/features/authentication/models/user.dart';
 import 'package:journal_app/features/shared/services/api_service.dart';
 import 'package:http/http.dart' as http;
 
+/// AuthService: authorization service that commincates with backend for user authorization.
 class AuthService extends ApiService with ChangeNotifier {
+  bool isLoggedIn = false;
+
+  /// register: registers a user via API call to backend.
   Future<http.Response> register({required User user}) async {
     http.Response response = await post(
       Endpoint.register.path,
       body: jsonEncode(user.toJSON()),
     );
 
+    if (response.statusCode == 200) {
+      isLoggedIn = true;
+      notifyListeners();
+    }
+
     return response;
   }
 
+  /// login: login a user via API call to backend.
   Future<http.Response> login({required String email, required String password}) async {
     http.Response response = await post(
       Endpoint.login.path,
       body: jsonEncode({"email": email, "password": password}),
     );
+
+    if (response.statusCode == 200) {
+      isLoggedIn = true;
+      notifyListeners();
+    }
 
     return response;
   }
