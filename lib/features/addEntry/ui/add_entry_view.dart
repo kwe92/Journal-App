f3b85ce44622cba0906fff3f65204612b22396a0
Kwe <Kweayon@gmail.com>
2023-11-28 10:07:39 -0600

    feat: implemented communication with backend successfully

--- lib/features/addEntry/ui/add_entry_view.dart
+++ lib/features/addEntry/ui/add_entry_view.dart
@@ -1,10 +1,14 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:journal_app/app/theme/colors.dart';
+import 'package:journal_app/app/app_router.gr.dart';
 import 'package:journal_app/app/theme/theme.dart';
+import 'package:journal_app/features/addEntry/ui/add_entry_view_model.dart';
+import 'package:journal_app/features/shared/models/new_entry.dart';
+import 'package:journal_app/features/shared/services/services.dart';
 import 'package:journal_app/features/shared/ui/base_scaffold.dart';
-
-// TODO: Work in view model | implement create entry function within JournalEntryService
+import 'package:journal_app/features/shared/ui/button/custom_back_button.dart';
+import 'package:journal_app/features/shared/ui/widgets/text_field_container.dart';
+import 'package:stacked/stacked.dart';
 
 @RoutePage()
 class AddEntryView extends StatelessWidget {
@@ -12,129 +16,72 @@ class AddEntryView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BaseScaffold(
-      title: "Add Entry",
-      // TODO: could be its own widget
-      leading: IconButton(
-        onPressed: () => Navigator.of(context).pop(),
-        icon: const Icon(
-          Icons.arrow_back,
-          color: Colors.white,
-          size: 32,
-        ),
-      ),
-      // TODO: refactor, same container in entry view
-      body: Column(
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height / 1.5,
-            // width: double.maxFinite,
-            margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
+    final TextEditingController newEntryController = TextEditingController();
+    final formKey = GlobalKey<FormState>();
 
-            // padding: EdgeInsets.only(bottom: 24),
-            decoration: const BoxDecoration(
-              color: AppColors.offWhite,
-              borderRadius: BorderRadius.all(
-                Radius.circular(16),
+    return ViewModelBuilder.reactive(
+      viewModelBuilder: () => AddEntryViewModel(),
+      builder: (context, model, _) => BaseScaffold(
+        title: "Add Entry",
+        leading: CustomBackButton(
+          onPressed: () => appRouter.replace(const JournalRoute()),
+        ),
+        body: Column(
+          children: [
+            TextFieldContainer(
+              height: MediaQuery.of(context).size.height / 1.5,
+              child: Form(
+                key: formKey,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
+                  child: TextFormField(
+                    // focus text field upon inital render
+                    autofocus: true,
+                    expands: true,
+                    maxLines: null,
+                    controller: newEntryController,
+                    decoration: borderlessInput,
+                    onChanged: (value) => model.setContent(value),
+                    // TODO: implemente validation
+                    // validator: (value) {},
+                  ),
+                ),
               ),
             ),
-            child: const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
-              child: TextField(
-                // keyboardType: TextInputType.text,
-                autofocus: true,
-                expands: true,
-                maxLines: null,
-                decoration: borderlessInput,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24.0),
-            child: SizedBox(
-              width: double.maxFinite,
-              // TODO: Add InkWell
-              child: Theme(
-                data: ThemeData(outlinedButtonTheme: offGreyButtonTheme),
-                child: OutlinedButton(
-                  onPressed: () async {
-                    // TODO: implement create entry
-                  },
-                  child: const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 12.0),
-                    child: Text(
-                      "Add Entry",
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: SizedBox(
+                width: double.maxFinite,
+                // TODO: Add InkWell
+                child: Theme(
+                  data: ThemeData(outlinedButtonTheme: offGreyButtonTheme),
+                  child: OutlinedButton(
+                    onPressed: () async {
+                      if (formKey.currentState!.validate() && model.ready) {
+                        await model.addEntry(NewEntry(content: model.content));
+                        model.clearContent();
+                        appRouter.replace(const JournalRoute());
+                      } else {
+                        // TODO: inform the user that the textfield can not be empty
+                      }
+                    },
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 12.0),
+                      child: Text("Add Entry"),
                     ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 }
 
-// @RoutePage()
-// class AddEntryView extends StatelessWidget {
-//   const AddEntryView({super.key});
+// Infinitly Long TextField
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return BaseScaffold(
-//       title: "Add Entry",
-//       // TODO: could be its own widget
-//       leading: IconButton(
-//         onPressed: () => Navigator.of(context).pop(),
-//         icon: const Icon(
-//           Icons.arrow_back,
-//           color: Colors.white,
-//           size: 32,
-//         ),
-//       ),
-//       // TODO: refactor, same container in entry view
-//       body: Column(
-//         children: [
-//           Container(
-//             // height: double.maxFinite,
-//             // width: double.maxFinite,
-//             margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
-
-//             // padding: EdgeInsets.only(bottom: 24),
-//             decoration: const BoxDecoration(
-//               color: AppColors.offWhite,
-//               borderRadius: BorderRadius.all(
-//                 Radius.circular(16),
-//               ),
-//             ),
-//             child: const Padding(
-//               padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
-//               child: TextField(
-//                 // keyboardType: TextInputType.text,
-//                 autofocus: true,
-//                 maxLines: 1,
-//                 decoration: borderlessInput,
-//               ),
-//             ),
-//           ),
-//           SizedBox(
-//             width: double.maxFinite,
-//             // TODO: Add InkWell
-//             child: OutlinedButton(
-//               onPressed: () async {
-//                 // TODO: implement create entry
-//               },
-//               child: const Padding(
-//                 padding: EdgeInsets.symmetric(vertical: 12.0),
-//                 child: Text(
-//                   "Add Entry",
-//                 ),
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
+//   - set TextField parameters:
+//       ~ expands: true
+//       ~ maxLines: null
---
+++ lib/features/addEntry/ui/add_entry_view_model.dart
@@ -0,0 +1,33 @@
+import 'package:http/http.dart';
+import 'package:journal_app/features/shared/models/new_entry.dart';
+import 'package:journal_app/features/shared/services/services.dart';
+import 'package:stacked/stacked.dart';
+
+class AddEntryViewModel extends BaseViewModel {
+  String? content;
+
+  // computed variable based on content state updated with change notifier
+  bool get ready {
+    return content != null && content!.isNotEmpty;
+  }
+
+  void setContent(String text) {
+    content = text;
+    notifyListeners();
+  }
+
+  void clearContent() {
+    content = null;
+
+    notifyListeners();
+  }
+
+  // TODO: add toast service here?
+  Future<Response> addEntry(NewEntry newEntry) async {
+    setBusy(true);
+    final Response response = await journalEntryService.addEntry(newEntry);
+    setBusy(false);
+
+    return response;
+  }
+}
