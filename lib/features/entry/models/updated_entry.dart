3730f00192355bda02938ec8bab353fb281d2321
Kwe <Kweayon@gmail.com>
2023-11-28 14:03:42 -0600

    feat: implemented update entry model

---
+++ lib/features/entry/models/updated_entry.dart
@@ -0,0 +1,18 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'updated_entry.g.dart';
+
+@JsonSerializable()
+class UpdatedEntry {
+  @JsonKey(name: "id")
+  int? entryId;
+  String? content;
+
+  UpdatedEntry({
+    required this.entryId,
+    required this.content,
+  });
+
+  factory UpdatedEntry.fromJSON(Map<String, dynamic> json) => _$UpdatedEntryFromJson(json);
+
+  Map<String, dynamic> toJSON() => _$UpdatedEntryToJson(this);
+}
---
+++ lib/features/entry/models/updated_entry.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'updated_entry.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UpdatedEntry _$UpdatedEntryFromJson(Map<String, dynamic> json) => UpdatedEntry(
+      entryId: json['id'] as int?,
+      content: json['content'] as String?,
+    );
+
+Map<String, dynamic> _$UpdatedEntryToJson(UpdatedEntry instance) =>
+    <String, dynamic>{
+      'id': instance.entryId,
+      'content': instance.content,
+    };
