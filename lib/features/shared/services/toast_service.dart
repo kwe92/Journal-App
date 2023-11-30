8749629021c4e5265da32f84b95c9ce1ab868b02
Kwe <Kweayon@gmail.com>
2023-11-30 14:46:12 -0600

    implemented genericpopupMenu / modal

--- lib/features/shared/services/toast_service.dart
+++ lib/features/shared/services/toast_service.dart
@@ -1,5 +1,10 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
+import 'package:journal_app/app/resources/reusables.dart';
+import 'package:journal_app/app/theme/colors.dart';
 import 'package:journal_app/features/shared/services/services.dart';
+import 'package:journal_app/features/shared/ui/button/selectable_button.dart';
+import 'package:journal_app/features/shared/utilities/popup_parameters.dart';
 
 /// ToastService: service for displaying user feedback upon success or failure of some event.
 /// Info boxes displayed include toasts, snackbars, banners and other temporary infomation popup boxes
@@ -17,7 +22,67 @@ class ToastService {
       ),
     );
   }
+
+  // TODO: implement modal as popUpMenu
+  // TODO: add modal popup parameters
+  // TODO: style modal
+
+  Future<T> popupMenu<T>(
+    BuildContext context, {
+    required PopupMenuParameters parameters,
+  }) async {
+    return await showDialog(
+            context: context,
+            builder: (context) {
+              return SimpleDialog(
+                title: Text(
+                  parameters.title,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 18, foreground: Paint()..color = AppColors.blueGrey0),
+                ),
+                children: [
+                  if (parameters.content != null) ...[
+                    Text(
+                      parameters.content!,
+                      textAlign: TextAlign.center,
+                    ),
+                    gap16
+                  ],
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Column(
+                      children: [
+                        ...parameters.options.entries.mapIndexed((int index, MapEntry buttonOption) {
+                          return index < parameters.options.entries.length - 1
+                              ? Column(
+                                  children: [
+                                    SelectableButton(
+                                      onPressed: () {
+                                        appRouter.pop(buttonOption.value);
+                                      },
+                                      label: buttonOption.key,
+                                    ),
+                                    gap12,
+                                  ],
+                                )
+                              : SelectableButton(
+                                  onPressed: () {
+                                    appRouter.pop(buttonOption.value);
+                                  },
+                                  label: buttonOption.key,
+                                );
+                        }).toList()
+                      ],
+                    ),
+                  )
+                ],
+              );
+            }) ??
+        parameters.defaultResult;
+  }
 }
+
+
 
 // ScaffoldMessenger
 
