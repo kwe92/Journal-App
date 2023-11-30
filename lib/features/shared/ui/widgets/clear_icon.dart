4f2e0d33dd11215b0283e281d55e983e2c9f18ca
Kwe <Kweayon@gmail.com>
2023-11-30 16:10:09 -0600

    feat: clear icon for text fields

---
+++ lib/features/shared/ui/widgets/clear_icon.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ConditionalClearIcon extends StatelessWidget {
+  final TextEditingController controller;
+  const ConditionalClearIcon({
+    required this.controller,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) => IconButton(
+        onPressed: () {
+          controller.clear();
+        },
+        icon: const Icon(Icons.clear),
+      );
+}
