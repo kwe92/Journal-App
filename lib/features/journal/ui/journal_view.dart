5bfdcb57a74ea40579339cecd6020be23dbcd37c
Kwe <Kweayon@gmail.com>
2023-12-10 13:49:56 -0600

    feat: implemented filter functionality

--- lib/features/journal/ui/journal_view.dart
+++ lib/features/journal/ui/journal_view.dart
@@ -4,7 +4,9 @@ import 'package:journal_app/app/app_router.gr.dart';
 import 'package:journal_app/app/resources/reusables.dart';
 import 'package:journal_app/features/journal/ui/journal_view_model.dart';
 import 'package:journal_app/features/journal/ui/widget/add_button.dart';
+import 'package:journal_app/features/journal/ui/widget/filter_button.dart';
 import 'package:journal_app/features/journal/ui/widget/journal_entry.dart';
+import 'package:journal_app/features/journal/ui/widget/side_menu.dart';
 import 'package:journal_app/features/shared/services/services.dart';
 import 'package:journal_app/features/shared/ui/base_scaffold.dart';
 import 'package:flutter/material.dart';
@@ -13,9 +15,7 @@ import 'package:stacked/stacked.dart';
 
 @RoutePage()
 class JournalView extends StatelessWidget {
-  JournalView({super.key});
-
-  final image = imageService.getRandomMindfulImage();
+  const JournalView({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -23,98 +23,65 @@ class JournalView extends StatelessWidget {
       viewModelBuilder: () => JournalViewModel(),
       onViewModelReady: (model) async {
         await model.initialize();
-        debugPrint("\njournal entries from JournalView: ${model.journalEntries}");
+
+        debugPrint("\nJournal entries from JournalView: ${model.journalEntries}");
       },
       // ! could a refresh method be used here instead of rebuilding the widget on insert?
       createNewViewModelOnInsert: true,
       builder: (context, model, child) {
         return BaseScaffold(
-          // title: "My Journel",
-
           // Thoughts in french
           title: "Pensées",
-          body: Center(
-            child: model.isBusy
-                ? circleLoader
-                : ListView.builder(
-                    // used to cented Text widget when there are no entries
-                    shrinkWrap: model.journalEntries.isEmpty ? true : false,
-                    itemCount: model.journalEntries.isEmpty ? 1 : model.journalEntries.length,
-                    itemBuilder: (BuildContext context, int i) {
-                      return model.journalEntries.isEmpty
-                          ? const Entry.opacity(
-                              duration: Duration(milliseconds: 600),
-                              child: Text(
-                                "No entries, whats on your mind...",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  color: AppColors.lightGreen,
-                                  fontSize: 32,
-                                ),
-                              ),
-                            )
-                          : Entry.opacity(
-                              duration: const Duration(milliseconds: 600),
-                              child: JournalEntry(
-                                index: i,
-                                journalEntry: model.journalEntries[i],
-                              ),
-                            );
-                    },
-                  ),
-          ),
-          // Open menu to the side
-          drawer: Drawer(
-            //CustomScrollView required to have Spacer / Expanded Widgets within a ListView
-            child: CustomScrollView(
-              slivers: [
-                SliverFillRemaining(
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        width: double.maxFinite,
-                        height: MediaQuery.of(context).size.height / 3.5,
-                        child: Image.asset(
-                          image,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      const Spacer(),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 8.0),
-                        child:
-                            // Use InkWell combined with Ink to respond to
-                            // user touch events and provide visual fedback
-                            InkWell(
-                          onTap: () async {
-                            // remove access token upon user logout
-                            await tokenService.removeAccessTokenFromStorage();
-
-                            // remove all routes and return to the signin page
-                            appRouter.pushAndPopUntil(SignInRoute(), predicate: (route) => false);
+          body: model.isBusy
+              ? circleLoader
+              : Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // TODO: disappear on scroll or make transparent somehow
+                    const Padding(
+                      padding: EdgeInsets.only(top: 16.0, right: 16),
+                      child: FilterButton(),
+                    ),
+                    Expanded(
+                      child: Center(
+                        child: ListView.builder(
+                          // used to center Text widget when there are no entries
+                          shrinkWrap: model.journalEntries.isEmpty ? true : false,
+                          itemCount: model.journalEntries.isEmpty ? 1 : model.journalEntries.length,
+                          itemBuilder: (BuildContext context, int i) {
+                            return model.journalEntries.isEmpty
+                                ? const Entry.opacity(
+                                    duration: Duration(milliseconds: 600),
+                                    child: Text(
+                                      "No entries, whats on your mind...",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        color: AppColors.lightGreen,
+                                        fontSize: 32,
+                                      ),
+                                    ),
+                                  )
+                                : Entry.opacity(
+                                    duration: const Duration(milliseconds: 600),
+                                    child: JournalEntry(
+                                      index: i,
+                                      journalEntry: model.journalEntries[i],
+                                    ),
+                                  );
                           },
-                          splashColor: AppColors.splashColor,
-                          highlightColor: AppColors.splashColor,
-                          child: Ink(
-                            child: const ListTile(
-                              leading: Icon(Icons.logout),
-                              title: Text("Logout"),
-                            ),
-                          ),
                         ),
                       ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
+                    ),
+                  ],
+                ),
+          // Open menu to the side
+          drawer: const SideMenu(),
           // BUTTON TO ADD NEW ENTRY
           floatingActionButton: AddButton(onTap: () {
-            // push add entry route and pop all routes to trigger createNewViewModelOnInsert
-
             appRouter.push(const MoodRoute());
 
+            // push add entry route and pop all routes to trigger createNewViewModelOnInsert
             // appRouter.pushAndPopUntil(const AddEntryRoute(), predicate: (route) => false);
           }),
         );
--- lib/features/journal/ui/journal_view_model.dart
+++ lib/features/journal/ui/journal_view_model.dart
@@ -1,11 +1,10 @@
+import 'package:journal_app/app/general/constants.dart';
 import 'package:journal_app/features/shared/models/entry.dart';
 import 'package:journal_app/features/shared/services/services.dart';
 import 'package:stacked/stacked.dart';
 
 class JournalViewModel extends BaseViewModel {
-  List<Entry> get journalEntries {
-    return journalEntryService.journalEntries;
-  }
+  List<Entry> journalEntries = [];
 
   Future<void> initialize() async {
     setBusy(true);
@@ -14,12 +13,50 @@ class JournalViewModel extends BaseViewModel {
     await Future.delayed(const Duration(seconds: 1));
 
     await journalEntryService.getAllEntries();
+
+    // initialize journalEntries with journalEntryService.journalEntries after backend call
+    journalEntries = journalEntryService.journalEntries;
+
     setBusy(false);
   }
 
-  Future<void> refresh() async {
-    setBusy(true);
-    await journalEntryService.getAllEntries();
-    setBusy(false);
+  void setFilteredJournalEntries(String mood) {
+    switch (mood) {
+      case 'all':
+        journalEntries = journalEntryService.journalEntries;
+        notifyListeners();
+        break;
+
+      case MoodType.awesome:
+        journalEntries = journalEntryService.journalEntries.where((entry) => entry.moodType == MoodType.awesome).toList();
+        notifyListeners();
+        break;
+
+      case MoodType.happy:
+        journalEntries = journalEntryService.journalEntries.where((entry) => entry.moodType == MoodType.happy).toList();
+        notifyListeners();
+        break;
+
+      case MoodType.okay:
+        journalEntries = journalEntryService.journalEntries.where((entry) => entry.moodType == MoodType.okay).toList();
+        notifyListeners();
+        break;
+
+      case MoodType.bad:
+        journalEntries = journalEntryService.journalEntries.where((entry) => entry.moodType == MoodType.bad).toList();
+        notifyListeners();
+        break;
+
+      case MoodType.terible:
+        journalEntries = journalEntryService.journalEntries.where((entry) => entry.moodType == MoodType.terible).toList();
+        notifyListeners();
+        break;
+    }
+
+    Future<void> refresh() async {
+      setBusy(true);
+      await journalEntryService.getAllEntries();
+      setBusy(false);
+    }
   }
 }
