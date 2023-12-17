5ad32c9a8b6b7d079a57803e1ffc04381b97a2a2
Kwe <Kweayon@gmail.com>
2023-12-17 17:23:50 -0600

    refact: cleaned up response handling

--- lib/features/authentication/ui/signUp/ui/signup_view_model.dart
+++ lib/features/authentication/ui/signUp/ui/signup_view_model.dart
@@ -4,8 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:journal_app/features/authentication/models/user.dart';
 import 'package:journal_app/features/authentication/ui/mixins/password_mixin.dart';
-import 'package:journal_app/features/shared/services/http_service.dart';
 import 'package:journal_app/features/shared/services/services.dart';
+import 'package:journal_app/features/shared/utilities/response_handler.dart';
 import 'package:stacked/stacked.dart';
 
 class SignUpViewModel extends BaseViewModel with PasswordMixin {
@@ -49,35 +49,17 @@ class SignUpViewModel extends BaseViewModel with PasswordMixin {
     final Response response = await authService.register(user: user);
     setBusy(false);
 
-    switch (response.statusCode) {
-      // failed status codes
-      case 209 || 400 || 401 || 403 || 550:
-        toastService.showSnackBar(
-          message: getErrorMsg(response.body),
-        );
+    final bool ok = ResponseHandler.checkStatusCode(response);
 
-        return false;
+    if (ok && authService.isLoggedIn) {
+      await tokenService.saveTokenData(
+        jsonDecode(response.body),
+      );
 
-      // success status codes
-      case 200 || 201:
-        if (authService.isLoggedIn) {
-          // upon successful registration retrieve jwt token from response
-          await tokenService.saveTokenData(
-            jsonDecode(response.body),
-          );
-
-          return true;
-
-          // remove member info view and navigate to journal view | there maybe a better way to refresh widget
-        } else {
-          toastService.showSnackBar();
-          return false;
-        }
-
-      default:
-        toastService.showSnackBar();
-        return false;
+      return ok;
     }
+
+    return ok;
   }
 
   void unfocusAll(BuildContext context) => toastService.unfocusAll(context);
