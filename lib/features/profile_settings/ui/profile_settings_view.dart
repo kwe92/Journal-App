014cdb92ebca142c77e7e77f7a3e220fd4e852bd
Kwe <Kweayon@gmail.com>
2023-12-18 15:46:27 -0600

    initial commit

---
+++ lib/features/profile_settings/ui/profile_settings_view.dart
@@ -0,0 +1,13 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class ProfileSettingsView extends StatelessWidget {
+  const ProfileSettingsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
