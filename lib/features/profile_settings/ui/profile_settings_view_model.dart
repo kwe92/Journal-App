a113223af32fd60cc823962345c1b6233498a3d9
Kwe <Kweayon@gmail.com>
2023-12-19 20:32:57 -0600

    feat: implemented ProfileSettingsView and ProfileSettingsViewModel

--- lib/features/profile_settings/ui/profile_settings_view_model.dart
+++ lib/features/profile_settings/ui/profile_settings_view_model.dart
@@ -1,3 +1,11 @@
+import 'package:journal_app/features/authentication/models/user.dart';
+import 'package:journal_app/features/shared/services/services.dart';
 import 'package:stacked/stacked.dart';
 
-class ProfileSettingsViewModel extends BaseViewModel {}
+class ProfileSettingsViewModel extends BaseViewModel {
+  User get currentUser => userService.currentUser!;
+
+  String get userFullName => "${currentUser.firstName ?? ''} ${currentUser.lastName ?? ''}";
+
+  String get userEmail => currentUser.email ?? '';
+}
