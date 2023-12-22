56404f1d2e66eccbf956f8db650b883da0aaaa54
Kwe <Kweayon@gmail.com>
2023-12-21 18:13:59 -0600

    feat: implemented DeleteProfileDialogViewModel

---
+++ lib/features/profile_settings/ui/delete_profile_dialog_view_model.dart
@@ -0,0 +1,40 @@
+// ignore_for_file: prefer_final_fields
+
+import 'package:flutter/material.dart';
+import 'package:journal_app/features/authentication/models/user.dart';
+import 'package:journal_app/features/shared/services/services.dart';
+import 'package:stacked/stacked.dart';
+
+class DeleteProfileDialogViewModel extends BaseViewModel {
+  // Mutable Variables
+  TextEditingController _confirmedEmailControllerController = TextEditingController();
+
+  String? _confirmedEmail;
+
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  // Computed Variables
+  String? get confirmedEmail => _confirmedEmail;
+
+  GlobalKey<FormState> get formKey => _formKey;
+
+  bool get emailMatch => confirmedEmail?.trim() == userEmail;
+
+  User get currentUser => userService.currentUser!;
+
+  String get userEmail => currentUser.email ?? '';
+
+  TextEditingController get confirmedEmailController => _confirmedEmailControllerController;
+
+  void setConfirmedEmail(String email) {
+    _confirmedEmail = email;
+    debugPrint('\nconfirmed email text: $email');
+    notifyListeners();
+  }
+
+  String? confirmValidAndMatchingEmail(String? confirmationEmail) {
+    final validationText = stringService.emailIsValid(confirmationEmail);
+
+    return validationText ?? (confirmationEmail != userEmail ? "emails do not match" : null);
+  }
+}
