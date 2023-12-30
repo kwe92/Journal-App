bb5b728e82228b166bdf296aa800f808e3e077cd
Kwe <Kweayon@gmail.com>
2023-12-29 19:03:03 -0600

    feat: implemented factory class

---
+++ lib/features/shared/factory/factory.dart
@@ -0,0 +1,40 @@
+import 'package:journal_app/features/authentication/models/user.dart';
+import 'package:journal_app/features/profile/edit_profile/model/updated_user.dart';
+import 'package:journal_app/features/shared/abstractions/base_user.dart';
+
+// TODO: add comments
+class Factory {
+  Factory._();
+
+  static BaseUser createUser({
+    required UserType userType,
+    String? firstName,
+    String? lastName,
+    String? email,
+    String? phoneNumber,
+    String? password,
+  }) {
+    switch (userType) {
+      case UserType.curentUser:
+        return User(
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          phoneNumber: phoneNumber,
+          password: password,
+        );
+      case UserType.updatedUser:
+        return UpdatedUser(
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          phoneNumber: phoneNumber,
+        );
+    }
+  }
+}
+
+enum UserType {
+  curentUser,
+  updatedUser,
+}
