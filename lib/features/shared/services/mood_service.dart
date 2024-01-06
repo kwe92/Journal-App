24c4bcb0bd4d1a2343c84061a16026e81abe4a53
Kwe <Kweayon@gmail.com>
2024-01-06 15:54:46 -0600

    feat: added todi

--- lib/features/shared/services/mood_service.dart
+++ lib/features/shared/services/mood_service.dart
@@ -4,22 +4,23 @@ import 'package:journal_app/features/shared/records/mood_record.dart';
 
 /// Encapsulates all data associated with mood type.
 class MoodService {
-  static const double _commonDefaultSize = 50.0;
+  static const double commonDefaultSize = 50.0;
 
-  static const double _moodAwesomeDefaultSize = 40.0;
+  static const double moodAwesomeDefaultSize = 40.0;
 
-  static const double _moodTerribleDefaultSize = 70.0;
+  static const double moodTerribleDefaultSize = 70.0;
 
   static final Map<String, MoodRecord> _moodsData = {
-    MoodType.awesome: (color: AppColors.moodAwesome, imagePath: MoodImagePath.moodAwesome, defaultSize: _moodAwesomeDefaultSize),
-    MoodType.happy: (color: AppColors.moodHappy, imagePath: MoodImagePath.moodHappy, defaultSize: _commonDefaultSize),
-    MoodType.okay: (color: AppColors.moodOkay, imagePath: MoodImagePath.moodOkay, defaultSize: _commonDefaultSize),
-    MoodType.bad: (color: AppColors.moodBad, imagePath: MoodImagePath.moodBad, defaultSize: _commonDefaultSize),
-    MoodType.terrible: (color: AppColors.moodTerrible, imagePath: MoodImagePath.moodTerrible, defaultSize: _moodTerribleDefaultSize),
+    MoodType.awesome: (color: AppColors.moodAwesome, imagePath: MoodImagePath.moodAwesome, defaultSize: moodAwesomeDefaultSize),
+    MoodType.happy: (color: AppColors.moodHappy, imagePath: MoodImagePath.moodHappy, defaultSize: commonDefaultSize),
+    MoodType.okay: (color: AppColors.moodOkay, imagePath: MoodImagePath.moodOkay, defaultSize: commonDefaultSize),
+    MoodType.bad: (color: AppColors.moodBad, imagePath: MoodImagePath.moodBad, defaultSize: commonDefaultSize),
+    MoodType.terrible: (color: AppColors.moodTerrible, imagePath: MoodImagePath.moodTerrible, defaultSize: moodTerribleDefaultSize),
   };
 
   final List<MapEntry<String, MoodRecord>> moods = _moodsData.entries.toList();
 
+  // TODO: refactor to return a map for testing
   MapEntry<String, MoodRecord> getMoodByType(String moodType) {
     return moods.where((moodMap) => moodMap.key == moodType).toList().first;
   }
