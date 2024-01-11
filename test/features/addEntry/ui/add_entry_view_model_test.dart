8ff7681e5b432e16f6f04585ab8b0c908ea8c250
Kwe <Kweayon@gmail.com>
2024-01-11 06:53:44 -0600

    feat: added new test case to test suite got currentUser method

--- test/features/addEntry/ui/add_entry_view_model_test.dart
+++ test/features/addEntry/ui/add_entry_view_model_test.dart
@@ -3,6 +3,7 @@ import 'package:http/http.dart';
 import 'package:journal_app/app/general/constants.dart';
 import 'package:journal_app/app/theme/colors.dart';
 import 'package:journal_app/features/addEntry/ui/add_entry_view_model.dart';
+import 'package:journal_app/features/shared/factory/factory.dart';
 import 'package:journal_app/features/shared/services/api_service.dart';
 import 'package:journal_app/features/shared/services/get_it.dart';
 import 'package:mockito/mockito.dart';
@@ -51,6 +52,32 @@ void main() {
       var actual = model.moodColor;
 
       var expected = AppColors.moodAwesome;
+
+      expect(actual, expected);
+    });
+
+    test('when user is authenticated, currentUser method returns correct user object', () {
+      // Arrange - Setup
+
+      final model = getModel();
+
+      final currentUser = AbstractFactory.createUser(
+        userType: UserType.curentUser,
+        firstName: 'Baki',
+        lastName: 'Hanma',
+        email: 'baki@grappler.io',
+        phoneNumber: '+11234567890',
+      );
+
+      getAndRegisterUserServiceMock(currentUser);
+
+      // Act
+
+      var actual = model.currentUser;
+
+      // Assert - Result
+
+      var expected = currentUser;
 
       expect(actual, expected);
     });
