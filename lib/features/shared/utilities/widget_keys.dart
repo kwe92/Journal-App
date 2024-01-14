1c0529c5c92d920570cb7602d1b70b7184f40912
Kwe <Kweayon@gmail.com>
2024-01-14 14:30:00 -0600

    feat: implemented WidgetKey static class

---
+++ lib/features/shared/utilities/widget_keys.dart
@@ -0,0 +1,10 @@
+// TODO: move all widget keys here
+
+import 'package:flutter/widgets.dart';
+
+class WidgetKey {
+  const WidgetKey._();
+
+  static GlobalObjectKey initialPasswordKey = const GlobalObjectKey('initial-password');
+  static GlobalObjectKey confirmPasswordKey = const GlobalObjectKey('confirm-password');
+}
