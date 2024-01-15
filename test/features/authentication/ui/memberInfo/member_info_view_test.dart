8ebf2943d91403c1d35bdc8bd39562079a8f49c8
Kwe <Kweayon@gmail.com>
2024-01-15 09:35:20 -0600

    feat: added test case but need to figure out why dragging buttons not working

--- test/features/authentication/ui/memberInfo/member_info_view_test.dart
+++ test/features/authentication/ui/memberInfo/member_info_view_test.dart
@@ -1,40 +1,92 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:journal_app/features/authentication/services/image_service.dart';
 import 'package:journal_app/features/authentication/ui/memberInfo/member_info_view.dart';
 import 'package:journal_app/features/shared/services/string_service.dart';
-import 'package:journal_app/features/shared/ui/button/selectable_button.dart';
+import 'package:journal_app/features/shared/utilities/widget_keys.dart';
 
 import '../../../../support/test_helpers.dart';
 
 void main() {
-  group('MemberInfoView - ', () {
-    setUp(() async => await registerSharedServices());
+  Future<void> pumpView(WidgetTester tester) async {
+    await tester.pumpWidget(
+      TestingWrapper.portal(
+        MemberInfoView(),
+      ),
+    );
+  }
 
-    testWidgets('When the MemberInfoView opens, Sign-up text and continue button visible - ', (tester) async {
-      // TODO: Review | overriding the default behavior of errors
-      FlutterError.onError = ignoreOverflowErrors;
+  group('MemberInfoView - ', () {
+    setUp(() async {
+      await registerSharedServices();
 
       TestWidgetsFlutterBinding.ensureInitialized();
 
       getAndRegisterService<ImageService>(ImageService());
-      getAndRegisterService<StringService>(StringService());
 
-      await tester.pumpWidget(
-        TestingWrapper.portal(
-          MemberInfoView(),
-        ),
-      );
+      getAndRegisterService<StringService>(StringService());
+    });
+
+    testWidgets("When the MemberInfoView opens, Sign-up text, TextFormField's, and continue button visible", (tester) async {
+      FlutterError.onError = ignoreOverflowErrors;
+
+      await pumpView(tester);
 
       // Finders
       final signUpFinder = find.text('Sign-up');
 
-      // TODO: figure out why you can't find descendants of CustomScrollView
-      // final testFinder = find.byType(CustomScrollView);
+      final firstNameTextFieldFinder = find.byKey(WidgetKey.firstNameKey, skipOffstage: false);
+
+      final lasttNameTextFieldFinder = find.byKey(WidgetKey.lastNameKey, skipOffstage: false);
+
+      final phoneNumberTextFieldFinder = find.byKey(WidgetKey.phoneNumberKey, skipOffstage: false);
+
+      final eemailTextFieldFinder = find.byKey(WidgetKey.emailKey, skipOffstage: false);
 
       expect(signUpFinder, findsWidgets);
 
-      // expect(testFinder, findsOneWidget);
+      expect(firstNameTextFieldFinder, findsWidgets);
+
+      expect(lasttNameTextFieldFinder, findsWidgets);
+
+      expect(phoneNumberTextFieldFinder, findsWidgets);
+
+      expect(eemailTextFieldFinder, findsWidgets);
+    });
+
+    testWidgets('when fields are empty and continue button pressed, then navigation does not happen', (tester) async {
+      FlutterError.onError = ignoreOverflowErrors;
+
+      await pumpView(tester);
+
+      // Finders
+
+      // final continueButtonFinder = find.byType(SelectableButton, skipOffstage: false);
+
+      final viewFinder = find.byType(CustomScrollView);
+
+      // TODO: figure out why SelectableButton is still off screen
+
+      // await tester.dragUntilVisible(
+      //   continueButtonFinder,
+      //   viewFinder,
+      //   const Offset(-500, 0),
+      // );
+
+      // final gesture = await tester.startGesture(Offset(400, 762)); //Position of the scrollview
+      // await gesture.moveBy(Offset(0, -300)); //How much to scroll by
+      // await tester.pump();
+
+      // expect(continueButtonFinder, findsOneWidget);
+
+      expect(viewFinder, findsOneWidget);
+
+      // expect(continueButtonFinder, findsOneWidget);
+
+      // await tester.pump();
+
+      // await tester.tap(continueButtonFinder);
     });
   });
 }
