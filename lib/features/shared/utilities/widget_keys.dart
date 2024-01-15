94203ca15ce1f35396e4c5a1b129765c4b4be622
Kwe <Kweayon@gmail.com>
2024-01-15 09:36:35 -0600

    feat: added more keys

--- lib/features/shared/utilities/widget_keys.dart
+++ lib/features/shared/utilities/widget_keys.dart
@@ -7,4 +7,8 @@ class WidgetKey {
 
   static GlobalObjectKey initialPasswordKey = const GlobalObjectKey('initial-password');
   static GlobalObjectKey confirmPasswordKey = const GlobalObjectKey('confirm-password');
+  static GlobalObjectKey firstNameKey = const GlobalObjectKey('first');
+  static GlobalObjectKey lastNameKey = const GlobalObjectKey('last');
+  static GlobalObjectKey phoneNumberKey = const GlobalObjectKey('phone');
+  static GlobalObjectKey emailKey = const GlobalObjectKey('email');
 }
