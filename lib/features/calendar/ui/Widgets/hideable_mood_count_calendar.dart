2fba50b4ffd0e9c7dac9c080ae29a8289d480ac8
Kwe <Kweayon@gmail.com>
2024-02-15 19:45:55 -0600

    feat: implemented HideableMoodCountCalendar

---
+++ lib/features/calendar/ui/Widgets/hideable_mood_count_calendar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:journal_app/app/general/constants.dart';
+import 'package:journal_app/features/calendar/ui/Widgets/filter_button_calendar.dart';
+import 'package:journal_app/features/calendar/ui/Widgets/mood_type_counter_calendar.dart';
+import 'package:journal_app/features/calendar/ui/calendar_view_model.dart';
+import 'package:stacked/stacked.dart';
+
+class HideableMoodCountCalendar extends ViewModelWidget<CalendarViewModel> {
+  const HideableMoodCountCalendar({super.key});
+
+  @override
+  Widget build(BuildContext context, CalendarViewModel viewModel) {
+    return SliverAppBar(
+      toolbarHeight: 32,
+      scrolledUnderElevation: 0,
+      automaticallyImplyLeading: false,
+      // floating: required to make SliverAppBar snappable
+      floating: true,
+      // snap: required to make SliverAppBar snappable
+      snap: true,
+      title: Padding(
+        padding: const EdgeInsets.only(left: 0, top: 8.0, right: 16),
+        child: Row(
+          children: [
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  MoodTypeCounterCalendar(moodType: MoodType.awesome.text, moodCount: viewModel.awesomeCount),
+                  MoodTypeCounterCalendar(moodType: MoodType.happy.text, moodCount: viewModel.happyCount),
+                  MoodTypeCounterCalendar(moodType: MoodType.okay.text, moodCount: viewModel.okayCount),
+                  MoodTypeCounterCalendar(moodType: MoodType.bad.text, moodCount: viewModel.badCount),
+                  MoodTypeCounterCalendar(moodType: MoodType.terrible.text, moodCount: viewModel.terribleCount),
+                ],
+              ),
+            ),
+            const FilterButtonCalendar()
+          ],
+        ),
+      ),
+    );
+  }
+}
