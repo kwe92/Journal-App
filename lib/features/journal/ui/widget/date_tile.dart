2de37ae9a80844513b02174a332bab53324631ce
Kwe <Kweayon@gmail.com>
2024-02-17 20:08:14 -0600

    refact: misc

--- lib/features/journal/ui/widget/date_tile.dart
+++ lib/features/journal/ui/widget/date_tile.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:journal_app/app/resources/reusables.dart';
 
 import 'package:journal_app/app/theme/colors.dart';
+import 'package:journal_app/features/shared/services/app_mode_service.dart';
 import 'package:journal_app/features/shared/services/services.dart';
+import 'package:provider/provider.dart';
 
 class DateTile extends StatelessWidget {
   final DateTime updatedAt;
@@ -14,36 +17,40 @@ class DateTile extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) => GestureDetector(
-        onTap: onPressed,
-        child: Container(
-          decoration: BoxDecoration(
-            color: AppColors.dateTileBackgroundColor,
-            borderRadius: const BorderRadius.all(
-              Radius.circular(8),
-            ),
-          ),
-          margin: const EdgeInsets.only(left: 16.0),
-          padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 8),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Icon(
-                Icons.calendar_month,
-                color: AppColors.dateTileColor,
-                size: 14,
-              ),
-              Text(
-                timeService.customDateString("dd MMM yyyy", updatedAt),
-                style: const TextStyle(
-                  fontSize: 12,
-                  fontWeight: FontWeight.w600,
-                  color: AppColors.dateTileColor,
-                ),
-              ),
-            ],
+  Widget build(BuildContext context) {
+    final isLightMode = context.watch<AppModeService>().isLightMode;
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        decoration: BoxDecoration(
+          color: AppColors.dateTileBackgroundColor,
+          borderRadius: const BorderRadius.all(
+            Radius.circular(8),
           ),
         ),
-      );
+        margin: const EdgeInsets.only(left: 16.0),
+        padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 8),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.calendar_month,
+              color: isLightMode ? AppColors.dateTileColor : Colors.white60,
+              size: 14,
+            ),
+            gap4,
+            Text(
+              timeService.customDateString("MM-dd-yyyy", updatedAt),
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w600,
+                color: isLightMode ? AppColors.dateTileColor : Colors.white60,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
