3216dd5d0694912f60a2bfc15297176191499f14
Kwe <Kweayon@gmail.com>
2024-02-18 23:00:24 -0600

    feat: initial quotes tests

---
+++ test/features/quotes/randomQuotes/ui/random_quotes_view_model_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../../support/test_helpers.dart';
+
+void main() {
+  group("RandomQuotesViewModel - ", () {
+    setUpAll(() async => await registerSharedServices());
+  });
+}
