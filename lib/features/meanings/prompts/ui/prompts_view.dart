e4b4406c6cd9dbc9d3bf4061f3a14935e1aec6f6
Kwe <Kweayon@gmail.com>
2024-02-25 18:39:30 -0600

    feat: updated ui

--- lib/features/meanings/prompts/ui/prompts_view.dart
+++ lib/features/meanings/prompts/ui/prompts_view.dart
@@ -15,50 +15,80 @@ class PromptsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: context.watch<AppModeService>().isLightMode ? Colors.white : Theme.of(context).colorScheme.background,
-      appBar: AppBar(
-        // backgroundColor: Theme.of(context).colorScheme.primary,
-        // backgroundColor: Colors.blue[200],
-        backgroundColor: AppColors.darkGrey0,
-
-        title: Text(
-          promptText,
-          style: const TextStyle(fontSize: 24),
-        ),
-      ),
       body: ChangeNotifierProvider<PromptsViewModel>(
         create: (context) => PromptsViewModel(promptText),
         builder: (context, _) {
           final model = context.watch<PromptsViewModel>();
 
-          return model.isBusy
-              ? Center(
-                  child: CircularProgressIndicator(
-                  // color: Colors.blue[200],
-                  color: Colors.deepPurpleAccent[100],
-                ))
-              : Center(
-                  child: Column(
-                    children: [
-                      Expanded(
-                        child: SingleChildScrollView(
-                          child: Column(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.all(24),
-                                child: Text(
-                                  model.promptTextResponse ?? "",
-                                  style: const TextStyle(fontSize: 18),
-                                ),
-                              ),
-                            ],
+          return CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                toolbarHeight: 56,
+                forceElevated: true,
+                shadowColor: AppColors.darkGrey1.withOpacity(0.25),
+                surfaceTintColor: Colors.white,
+                backgroundColor: context.watch<AppModeService>().isLightMode ? Colors.white : AppColors.darkGrey1,
+                title: Text(
+                  promptText,
+                  style: const TextStyle(fontSize: 24),
+                ),
+              ),
+              model.isBusy
+                  ? SliverToBoxAdapter(
+                      child: Padding(
+                        padding: EdgeInsets.only(top: MediaQuery.of(context).size.height / 2.75),
+                        child: Center(
+                          child: CircularProgressIndicator(
+                            // color: Colors.blue[200],
+                            color: Colors.deepPurpleAccent[100],
                           ),
                         ),
                       ),
-                    ],
-                  ),
-                );
+                    )
+                  : SliverToBoxAdapter(
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(24),
+                              child: Text(
+                                model.promptTextResponse ?? "",
+                                style: const TextStyle(fontSize: 18),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+            ],
+          );
         },
       ),
     );
   }
 }
+
+
+
+
+// Center(
+//                   child: Column(
+//                     children: [
+//                       Expanded(
+//                         child: SingleChildScrollView(
+//                           child: Column(
+//                             children: [
+//                               Padding(
+//                                 padding: const EdgeInsets.all(24),
+//                                 child: Text(
+//                                   model.promptTextResponse ?? "",
+//                                   style: const TextStyle(fontSize: 18),
+//                                 ),
+//                               ),
+//                             ],
+//                           ),
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                 );
