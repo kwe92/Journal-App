37e6fa49330a42fb046ffc208d1bc49af9feca10
Kwe <Kweayon@gmail.com>
2024-02-27 13:44:32 -0600

    refact: was having issues using a dialog widget so refactored into a view with scaffold

--- lib/features/profile/profile_settings/ui/delete_profile_dialog_view.dart
+++ lib/features/profile/profile_settings/ui/delete_profile_dialog_view.dart
@@ -8,6 +8,7 @@ import 'package:journal_app/features/profile/profile_settings/ui/delete_profile_
 import 'package:journal_app/features/shared/services/app_mode_service.dart';
 import 'package:journal_app/features/shared/services/services.dart';
 import 'package:journal_app/features/shared/ui/button/selectable_button.dart';
+import 'package:journal_app/features/shared/utilities/device_size.dart';
 import 'package:journal_app/features/shared/utilities/popup_parameters.dart';
 import 'package:provider/provider.dart';
 import 'package:stacked/stacked.dart';
@@ -22,115 +23,238 @@ class DeleteProfileDialogView extends StatelessWidget {
     return ViewModelBuilder<DeleteProfileDialogViewModel>.reactive(
       viewModelBuilder: () => DeleteProfileDialogViewModel(),
       builder: (BuildContext context, DeleteProfileDialogViewModel model, _) {
-        const style00 = TextStyle(fontSize: 12);
+        final smallDevice = DeviceSize.isSmallDevice(context);
 
-        return SimpleDialog(
+        const style00 = TextStyle(fontSize: 12);
+        return Scaffold(
           backgroundColor: Theme.of(context).colorScheme.background,
-          surfaceTintColor: Colors.white,
-          titlePadding: const EdgeInsets.fromLTRB(16.0, 24.0, 16.0, 0.0),
-          contentPadding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 16.0),
-          title: Text(
-            parameters.title,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 18,
-              foreground: Paint()..color = isLightMode ? AppColors.blueGrey0 : Colors.white,
+          body: Padding(
+            padding: const EdgeInsets.only(
+              left: 24,
+              top: 24,
+              right: 24,
             ),
-          ),
-          children: [
-            if (parameters.content != null) ...[
-              Text(
-                parameters.content!,
-                textAlign: TextAlign.center,
-                style: const TextStyle(fontSize: 14),
-              ),
-              gap16,
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+            child: Center(
+              child: ListView(
+                shrinkWrap: true,
                 children: [
-                  const Text(
-                    'type ',
-                    style: style00,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 6),
-                    decoration: BoxDecoration(
-                      color: AppColors.offGrey.withOpacity(0.15),
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(4),
+                  if (parameters.content != null) ...[
+                    Text(
+                      parameters.title.toUpperCase(),
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        foreground: Paint()..color = isLightMode ? AppColors.blueGrey0 : Colors.white,
+                        fontSize: !smallDevice ? 18 : 14,
+                        fontWeight: FontWeight.w700,
                       ),
                     ),
-                    child: Text(
-                      model.userEmail,
-                      style: style00.copyWith(
-                        fontWeight: FontWeight.w600,
+                    gap18,
+                    Text(
+                      parameters.content!,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(fontSize: 14),
+                    ),
+                    gap18,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'type ',
+                          style: style00,
+                        ),
+                        Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 6),
+                          decoration: BoxDecoration(
+                            color: AppColors.offGrey.withOpacity(0.15),
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(4),
+                            ),
+                          ),
+                          child: Text(
+                            model.userEmail,
+                            style: style00.copyWith(
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                        const Text(
+                          ' to proceed',
+                          style: style00,
+                        ),
+                      ],
+                    ),
+                    gap18,
+                    Form(
+                      key: model.formKey,
+                      child: Theme(
+                        data: Theme.of(context).copyWith(
+                          inputDecorationTheme: deleteAccoutInputTheme,
+                          textSelectionTheme: deleteAccoutTextSelectionTheme,
+                        ),
+                        child: TextFormField(
+                          controller: model.confirmedEmailController,
+                          autofillHints: const [AutofillHints.email],
+                          validator: model.confirmValidAndMatchingEmail,
+                          onChanged: model.setConfirmedEmail,
+                          decoration: const InputDecoration(
+                            labelText: 'Email',
+                            hintText: 'Confirm Email',
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                  const Text(
-                    ' to proceed',
-                    style: style00,
-                  ),
+                    gap18,
+                  ],
+                  Column(
+                    children: [
+                      ...parameters.options.entries.mapIndexed((int index, MapEntry buttonOption) {
+                        return index < parameters.options.entries.length - 1
+                            ? Column(
+                                children: [
+                                  SelectableButton(
+                                    color: model.emailMatch ? AppColors.lightGreen : AppColors.offGrey,
+                                    onPressed: () async {
+                                      if (model.formKey.currentState!.validate() && model.emailMatch) {
+                                        final bool accountDeletedSuccessfully = await model.deleteAccount();
+
+                                        if (accountDeletedSuccessfully) {
+                                          debugPrint('user permanently deleted, farewell my friend: ${model.userEmail}');
+
+                                          await model.cleanResources();
+
+                                          appRouter.pushAndPopUntil(const FarewellRoute(), predicate: (route) => false);
+                                        }
+                                      }
+                                    },
+                                    label: buttonOption.key,
+                                  ),
+                                  gap12,
+                                ],
+                              )
+                            : SelectableButton(
+                                onPressed: () => appRouter.pop(buttonOption.value),
+                                label: buttonOption.key,
+                              );
+                      }).toList()
+                    ],
+                  )
                 ],
               ),
-              gap16,
-              Form(
-                key: model.formKey,
-                child: Theme(
-                  data: Theme.of(context).copyWith(
-                    inputDecorationTheme: deleteAccoutInputTheme,
-                    textSelectionTheme: deleteAccoutTextSelectionTheme,
-                  ),
-                  child: TextFormField(
-                    controller: model.confirmedEmailController,
-                    autofillHints: const [AutofillHints.email],
-                    validator: model.confirmValidAndMatchingEmail,
-                    onChanged: model.setConfirmedEmail,
-                    decoration: const InputDecoration(
-                      labelText: 'Email',
-                      hintText: 'Confirm Email',
-                    ),
-                  ),
-                ),
-              ),
-              gap16,
-            ],
-            Column(
-              children: [
-                ...parameters.options.entries.mapIndexed((int index, MapEntry buttonOption) {
-                  return index < parameters.options.entries.length - 1
-                      ? Column(
-                          children: [
-                            SelectableButton(
-                              color: model.emailMatch ? AppColors.lightGreen : AppColors.offGrey,
-                              onPressed: () async {
-                                if (model.formKey.currentState!.validate() && model.emailMatch) {
-                                  final bool accountDeletedSuccessfully = await model.deleteAccount();
-
-                                  if (accountDeletedSuccessfully) {
-                                    debugPrint('user permanently deleted, farewell my friend: ${model.userEmail}');
-
-                                    await model.cleanResources();
-
-                                    appRouter.pushAndPopUntil(const FarewellRoute(), predicate: (route) => false);
-                                  }
-                                }
-                              },
-                              label: buttonOption.key,
-                            ),
-                            gap12,
-                          ],
-                        )
-                      : SelectableButton(
-                          onPressed: () => appRouter.pop(buttonOption.value),
-                          label: buttonOption.key,
-                        );
-                }).toList()
-              ],
-            )
-          ],
+            ),
+          ),
         );
       },
     );
   }
 }
+
+
+// ! Old Dialog Widget
+
+// SimpleDialog(
+//           backgroundColor: Theme.of(context).colorScheme.background,
+//           surfaceTintColor: Colors.white,
+//           titlePadding: const EdgeInsets.fromLTRB(16.0, 24.0, 16.0, 0.0),
+//           contentPadding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 16.0),
+//           title: Text(
+//             parameters.title,
+//             textAlign: TextAlign.center,
+//             style: TextStyle(
+//               fontSize: 18,
+//               foreground: Paint()..color = isLightMode ? AppColors.blueGrey0 : Colors.white,
+//             ),
+//           ),
+//           children: [
+//             if (parameters.content != null) ...[
+//               Text(
+//                 parameters.content!,
+//                 textAlign: TextAlign.center,
+//                 style: const TextStyle(fontSize: 14),
+//               ),
+//               gap16,
+//               Flex(
+//                 direction: !DeviceSize.isSmallDevice(context) ? Axis.horizontal : Axis.vertical,
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: [
+//                   const Text(
+//                     'type ',
+//                     style: style00,
+//                   ),
+//                   Container(
+//                     padding: const EdgeInsets.symmetric(horizontal: 6),
+//                     decoration: BoxDecoration(
+//                       color: AppColors.offGrey.withOpacity(0.15),
+//                       borderRadius: const BorderRadius.all(
+//                         Radius.circular(4),
+//                       ),
+//                     ),
+//                     child: Text(
+//                       model.userEmail,
+//                       style: style00.copyWith(
+//                         fontWeight: FontWeight.w600,
+//                       ),
+//                     ),
+//                   ),
+//                   const Text(
+//                     ' to proceed',
+//                     style: style00,
+//                   ),
+//                 ],
+//               ),
+//               gap16,
+//               Form(
+//                 key: model.formKey,
+//                 child: Theme(
+//                   data: Theme.of(context).copyWith(
+//                     inputDecorationTheme: deleteAccoutInputTheme,
+//                     textSelectionTheme: deleteAccoutTextSelectionTheme,
+//                   ),
+//                   child: TextFormField(
+//                     controller: model.confirmedEmailController,
+//                     autofillHints: const [AutofillHints.email],
+//                     validator: model.confirmValidAndMatchingEmail,
+//                     onChanged: model.setConfirmedEmail,
+//                     decoration: const InputDecoration(
+//                       labelText: 'Email',
+//                       hintText: 'Confirm Email',
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//               gap16,
+//             ],
+//             Column(
+//               children: [
+//                 ...parameters.options.entries.mapIndexed((int index, MapEntry buttonOption) {
+//                   return index < parameters.options.entries.length - 1
+//                       ? Column(
+//                           children: [
+//                             SelectableButton(
+//                               color: model.emailMatch ? AppColors.lightGreen : AppColors.offGrey,
+//                               onPressed: () async {
+//                                 if (model.formKey.currentState!.validate() && model.emailMatch) {
+//                                   final bool accountDeletedSuccessfully = await model.deleteAccount();
+
+//                                   if (accountDeletedSuccessfully) {
+//                                     debugPrint('user permanently deleted, farewell my friend: ${model.userEmail}');
+
+//                                     await model.cleanResources();
+
+//                                     appRouter.pushAndPopUntil(const FarewellRoute(), predicate: (route) => false);
+//                                   }
+//                                 }
+//                               },
+//                               label: buttonOption.key,
+//                             ),
+//                             gap12,
+//                           ],
+//                         )
+//                       : SelectableButton(
+//                           onPressed: () => appRouter.pop(buttonOption.value),
+//                           label: buttonOption.key,
+//                         );
+//                 }).toList()
+//               ],
+//             )
+//           ],
+//         );
