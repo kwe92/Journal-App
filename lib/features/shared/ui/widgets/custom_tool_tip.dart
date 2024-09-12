bd5a7d4aab9ae21d5c18b89e2e5bcba4f61bac11
Kwe <Kweayon@gmail.com>
2024-09-12 09:27:44 -0500

    refact: changed padding parameter and added stadium background

--- lib/features/shared/ui/widgets/custom_tool_tip.dart
+++ lib/features/shared/ui/widgets/custom_tool_tip.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:journal_app/features/shared/services/app_mode_service.dart';
-import 'package:provider/provider.dart';
 
 class CustomToolTip extends StatelessWidget {
+  final Color? backgroundColor;
   final String message;
   final Widget child;
   final EdgeInsets? margin;
   const CustomToolTip({
+    this.backgroundColor,
     required this.message,
     required this.child,
     this.margin = const EdgeInsets.only(top: 4),
@@ -15,18 +15,22 @@ class CustomToolTip extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    bool isLightMode = context.watch<AppModeService>().isLightMode;
-
     return Tooltip(
       margin: margin,
-      textStyle: TextStyle(
-        color: isLightMode ? Colors.black : Colors.white,
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      textStyle: const TextStyle(
+        color: Colors.black,
         fontSize: 16,
         fontWeight: FontWeight.w600,
       ),
-      decoration: const BoxDecoration(
-        color: Colors.transparent,
-      ),
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(16), color: backgroundColor ?? Colors.lightBlue, boxShadow: [
+        BoxShadow(
+          color: Colors.black.withOpacity(0.15),
+          offset: const Offset(0, 2),
+          spreadRadius: 2,
+          blurRadius: 4,
+        )
+      ]),
       message: message,
       child: child,
     );
