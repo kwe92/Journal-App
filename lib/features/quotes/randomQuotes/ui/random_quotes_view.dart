f6140f8960ea3ea1754fa5c39252a2ee4e27c511
Kwe <Kweayon@gmail.com>
2024-10-08 10:27:33 -0500

    refact: updated view
    	- renamed model to viewModel
    
    	- removed unnecessary SizedBox
    
    	- added rive heart animation

--- lib/features/quotes/randomQuotes/ui/random_quotes_view.dart
+++ lib/features/quotes/randomQuotes/ui/random_quotes_view.dart
@@ -6,11 +6,11 @@ import 'package:journal_app/app/resources/reusables.dart';
 import 'package:journal_app/app/theme/colors.dart';
 import 'package:journal_app/features/quotes/randomQuotes/ui/random_quotes_view_model.dart';
 import 'package:journal_app/features/quotes/shared/utils/functions.dart';
-import 'package:journal_app/features/quotes/shared/widgets/favorite_button.dart';
 import 'package:journal_app/features/quotes/shared/widgets/share_button.dart';
 import 'package:journal_app/features/shared/services/app_mode_service.dart';
 import 'package:journal_app/features/shared/services/services.dart';
 import 'package:provider/provider.dart';
+import 'package:rive/rive.dart';
 
 @RoutePage()
 class RandomQuotesView extends StatelessWidget {
@@ -20,67 +20,64 @@ class RandomQuotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     final style = TextStyle(fontSize: 24, fontFamily: FontFamily.playwrite.name);
 
-    final model = context.watch<RandomQuotesViewModel>();
+    final viewModel = context.watch<RandomQuotesViewModel>();
 
     final smallDevice = deviceSizeService.smallDevice;
 
-    return model.isBusy
+    return viewModel.isBusy
         ? circleLoader
         : PageView.builder(
             scrollDirection: Axis.vertical,
-            itemCount: model.quotes.length,
+            itemCount: viewModel.quotes.length,
             itemBuilder: (context, index) {
               return Stack(
                 children: [
                   Center(
-                    // TODO: find out why this sized box is here
-                    child: SizedBox(
-                      height: MediaQuery.of(context).size.height / 1.5,
-                      child: Stack(
-                        children: [
-                          Positioned.fill(
-                            // TODO: review scaleY and scaleX the numbers appear arbitrary
-                            child: Transform.scale(
-                              scaleY: 1.325,
-                              scaleX: 1.0625,
-                              child: SvgPicture.asset(
-                                "assets/images/quotes-background-image.svg",
-                                color: AppColors.darkGrey0.withOpacity(context.read<AppModeService>().isLightMode ? 0.15 : 1),
-                              ),
+                    child: Stack(
+                      children: [
+                        Positioned.fill(
+                          // scales child (inthis case an image) along the 2D plane
+                          child: Transform.scale(
+                            scaleY: 1.325,
+                            scaleX: 1.0625,
+                            child: SvgPicture.asset(
+                              "assets/images/quotes-background-image.svg",
+                              color: AppColors.darkGrey0.withOpacity(context.read<AppModeService>().isLightMode ? 0.15 : 1),
                             ),
                           ),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 32),
-                                child: Align(
-                                  child: Text(
-                                    model.quotes[index].quote,
-                                    style: style,
-                                    textAlign: TextAlign.center,
-                                  ),
-                                ),
-                              ),
-                              !smallDevice ? gap18 : gap12,
-                              Align(
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 32),
+                              child: Align(
                                 child: Text(
-                                  "- ${model.quotes[index].author}",
-                                  style: style.copyWith(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.w600,
-                                  ),
+                                  viewModel.quotes[index].quote,
+                                  style: style,
+                                  textAlign: TextAlign.center,
                                 ),
                               ),
-                            ],
-                          ),
-                        ],
-                      ),
+                            ),
+                            !smallDevice ? gap18 : gap12,
+                            Align(
+                              child: Text(
+                                "- ${viewModel.quotes[index].author}",
+                                style: style.copyWith(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                   ),
                   Padding(
                     padding: EdgeInsets.only(bottom: !smallDevice ? 42 : 12),
                     child: Align(
+                      // align child to bottom instead of guessing with random spaces
                       alignment: Alignment.bottomCenter,
                       child: Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 24),
@@ -89,12 +86,20 @@ class RandomQuotesView extends StatelessWidget {
                           children: [
                             ShareButton(
                               size: 52.0,
-                              contentToShare: shareQuote(model.quotes[index]),
+                              contentToShare: shareQuote(viewModel.quotes[index]),
                             ),
-                            FavoriteButton(
-                              onPressed: () => model.setLikedForQuote(model.quotes[index]),
-                              size: 52,
-                              isLiked: model.quotes[index].isLiked,
+                            GestureDetector(
+                              onTap: () async {
+                                await viewModel.setLikedForQuote(viewModel.quotes[index]);
+                              },
+                              child: SizedBox(
+                                width: 52.0,
+                                height: 52.0,
+                                child: Transform.scale(
+                                  scale: 5.25,
+                                  child: Rive(artboard: viewModel.riveArtBoard ?? RuntimeArtboard().artboard),
+                                ),
+                              ),
                             ),
                           ],
                         ),
