bd0452d43798ad137dc18e0078965c55c6544e90
Kwe <Kweayon@gmail.com>
2024-10-11 18:07:38 -0500

    task: updated view model
    	- implemented initialize method
    
    	- implemented setFilteredLikedQuotes method

--- lib/features/quotes/likedQuotes/ui/liked_quotes_view_model.dart
+++ lib/features/quotes/likedQuotes/ui/liked_quotes_view_model.dart
@@ -6,47 +6,51 @@ import 'package:journal_app/features/shared/services/services.dart';
 import 'package:stacked/stacked.dart';
 
 class LikedQuotesViewModel extends ReactiveViewModel {
-  List<LikedQuote> get likedQuotes => likedQuotesService.sortedLikedQuotes;
+  List<LikedQuote> _likedQuotes = [];
+
+  List<LikedQuote> get likedQuotes => _likedQuotes;
+
+  void initialize() {
+    _likedQuotes = likedQuotesService.sortedLikedQuotes;
+  }
+
+  //!! TODO: use enum instead of a string in all the palces where the word all is used to control the liked quotes filter
 
   var modelQuery = ValueNotifier<String>('all');
-
-  @override
-  List<ListenableServiceMixin> get listenableServices => [likedQuotesService];
 
   Future<void> deleteLikedQuote(LikedQuote quote) async {
     // set liked to false
     quote.isLiked = !quote.isLiked;
 
     await likedQuotesService.deleteLikedQuote(quote);
+
+    var numberOfQuotes =
+        likedQuotes.where((likedQuote) => likedQuote.author.trim().toLowerCase() == quote.author.trim().toLowerCase()).length;
+
+    if (numberOfQuotes == 1) {
+      const query = 'All';
+
+      modelQuery.value = query;
+
+      setFilteredLikedQuotes(modelQuery.value);
+
+      return;
+    }
+
+    setFilteredLikedQuotes(modelQuery.value);
   }
 
-  // void setFilteredLikedQuotes(String query) {
-  //   modelQuery.value = query;
+  void setFilteredLikedQuotes(String query) {
+    modelQuery.value = query;
 
-  //   if (query.trim().toLowerCase() == 'all') {
-  //     likedQuotes = likedQuotesService.likedQuotes;
-  //     notifyListeners();
+    if (query.trim().toLowerCase() == 'all') {
+      _likedQuotes = likedQuotesService.likedQuotes;
+      notifyListeners();
 
-  //     return;
-  //   }
-  //   likedQuotes = likedQuotesService.likedQuotes.where((quote) => quote.author.trim().toLowerCase() == query.trim().toLowerCase()).toList();
-  //   notifyListeners();
-  // }
+      return;
+    }
+    _likedQuotes =
+        likedQuotesService.likedQuotes.where((quote) => quote.author.trim().toLowerCase() == query.trim().toLowerCase()).toList();
+    notifyListeners();
+  }
 }
-
-
-    // var numberOfQuotes =
-    //     likedQuotes.where((likedQuote) => likedQuote.author.trim().toLowerCase() == quote.author.trim().toLowerCase()).length;
-
-    // // remove quote from list of liked quotes
-    // likedQuotes.remove(quote);
-
-    // if (numberOfQuotes == 1) {
-    //   const query = 'All';
-
-    //   modelQuery.value = query;
-
-    //   setFilteredLikedQuotes(query);
-    // }
-
-    // notifyListeners();
