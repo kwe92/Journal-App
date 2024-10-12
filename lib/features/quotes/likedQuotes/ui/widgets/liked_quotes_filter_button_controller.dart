97daaef109d7eac2d3cdf654b6c1bdf21741a644
Kwe <Kweayon@gmail.com>
2024-10-12 12:04:50 -0500

    task: updated LikedQuotesFilterButtonController
    	- alphabetized list of filterable author names
    
    	- used enum instead of hard coded string

--- lib/features/quotes/likedQuotes/ui/widgets/liked_quotes_filter_button_controller.dart
+++ lib/features/quotes/likedQuotes/ui/widgets/liked_quotes_filter_button_controller.dart
@@ -1,20 +1,23 @@
+import 'package:journal_app/app/general/constants.dart';
 import 'package:journal_app/features/shared/services/services.dart';
 import 'package:stacked/stacked.dart';
 
 class LikedQuotesFilterButtonController extends ReactiveViewModel {
-  String? _dropdownValue = 'All';
+  String? _dropdownValue = QuoteAuthorFilterOptions.all.name;
 
   String? get dropdownValue => _dropdownValue;
 
   List<String> get dropdownOptions {
     final authorNames =
+        // when likedQuotesService.likedQuotes changes this getter is recomputed
         // ignore: prefer_interpolation_to_compose_strings
         likedQuotesService.likedQuotes.fold<String>("", (initialValue, quote) => initialValue + quote.author + ", ").split(", ");
 
     authorNames.removeLast();
+    // sort in alphabetical order
+    authorNames.sort((a, b) => a.compareTo(b));
 
-    //!! TODO: sort by name
-    return ['All', ...authorNames.toSet()];
+    return [QuoteAuthorFilterOptions.all.name, ...authorNames.toSet()];
   }
 
   @override
